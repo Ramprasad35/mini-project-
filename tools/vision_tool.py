4fbb98ee26ac49bf0deed5bf053bb04a972f2a61
Ram <ramprasadsk2020@gmail.com>
2026-02-23 18:33:01 +0530

    updated ai-agent

--- tools/vision_tool.py
+++ tools/vision_tool.py
@@ -4,3 +4,8 @@ def vision_tool(image_path):
 
     except Exception as  e :
         return f"Error analyzing image:{str(e)}"
+    
+if __name__ == "__main__":
+    image_path = input("Enter image path: ")
+    result = vision_tool(image_path)
+    print(result)

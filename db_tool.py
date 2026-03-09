cb3a4c41d40ba1ec46c9eb334036d9d6066bad21
Ram <ramprasadsk2020@gmail.com>
2026-03-09 18:25:16 +0530

    new version of ai agent with neo4j

--- db_tool.py
+++ db_tool.py
@@ -18,3 +18,13 @@ def store_image_result(user_name, image_name ,label):
         
         return "Data stored successully in Neo4j" 
 
+def get_user_images(user_name):
+    with driver.session() as session:
+        result = session.run(
+            """Match(u:User{name:$user_name})-[:UPLOADED]->(i:Image)
+            RETURN i.name AS image_name
+            """,
+            user_name=user_name
+        )         
+        images = [record["images_name"]for record in result]
+        return images                                               
